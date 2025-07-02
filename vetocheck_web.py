f2532d3476d240e75ce6d4b02eecc7d70e0acb09
Dicode <119669799+hackintux@users.noreply.github.com>
2025-07-02 13:34:24 +0200

    Update vetocheck_web.py

--- vetocheck_web.py
+++ vetocheck_web.py
@@ -76,8 +76,8 @@ with col2:
 st.markdown("---")
 
 # === CHOIX CHIEN / CHAT ===
-st.subheader("🐶 / 🐱 / 🐰 / 👩‍🦰 Quel animal souhaitez-vous diagnostiquer ?")
-animal_type = st.radio("Type d'animal :", ["Chien", "Chat", "Lapin", "Maeva"], horizontal=True)
+st.subheader("🐶 / 🐱 / 🐰  Quel animal souhaitez-vous diagnostiquer ?")
+animal_type = st.radio("Type d'animal :", ["Chien", "Chat", "Lapin"], horizontal=True)
 
 # === QUESTIONS ===
 if animal_type:
